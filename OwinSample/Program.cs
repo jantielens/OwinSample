9dd724956458477675c012ba461365173f82ba80
jantielens <j@tlns.be>
2014-11-21 21:31:36 +0100

    published to blog

--- OwinSample/Program.cs
+++ OwinSample/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Owin.Hosting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,13 @@ namespace OwinSample
     {
         static void Main(string[] args)
         {
+            string baseUrl = "http://*:5000";
+            using (WebApp.Start<Startup>(baseUrl))
+            {
+                Console.WriteLine("Press Enter to quit.");
+                Console.ReadKey();
+
+            }
         }
     }
 }
---
+++ OwinSample/Startup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(OwinSample.Startup))]
+
+namespace OwinSample
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseWelcomePage(new Microsoft.Owin.Diagnostics.WelcomePageOptions()
+            {
+                Path = new PathString("/welcome")
+            });
+
+            app.Run(context =>
+            {
+                context.Response.ContentType = "text/plain";
+
+                string output = string.Format(
+                    "I'm running on {0} \nFrom assembly {1}", 
+                    Environment.OSVersion, 
+                    System.Reflection.Assembly.GetEntryAssembly().FullName
+                    );
+
+                return context.Response.WriteAsync(output);
+
+            });
+        }
+    }
+}
